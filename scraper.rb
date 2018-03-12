359bfd3926640516b46090fd9e01344c2d33462f
WaltersL <lynton.walters@yahoo.com.au>
2018-03-12 16:13:53 +1100

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -16,8 +16,6 @@ p page.at('div.content')
 ScraperWiki.save_sqlite(["Tests"], {"name" => "South Africa v Australia", "venue"=> "at Cape Town"})
 #
 # # An arbitrary query against the database
-ScraperWiki.select("* from data where 'name'='peter'")
-
 # You don't have to do things with the Mechanize or ScraperWiki libraries.
 # You can use whatever gems you want: https://morph.io/documentation/ruby
 # All that matters is that your final data is written to an SQLite database
